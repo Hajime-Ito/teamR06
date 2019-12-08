6ce0ff65caaa48ad3ef7d2ea009252b803c6db4f
hebereke <antny0202@gmail.com>
2019-12-08 12:58:00 +0900

    テンプレートプロジェクトの追加とgitignoreの追加

---
+++ client/p2hacks_2019_team06_client/p2hacks_2019_team06_client/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace p2hacks_2019_team06_client
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
