28833a8a606d941087ce5bc8a79770a9336977b4
Hiro <Hiro@STB1018113>
2019-12-09 10:52:51 +0900

    基本的な関数を仮実装

--- cliant/cliant/SerberDataManager.cs
+++ cliant/cliant/SerberDataManager.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace cliant
 {
-    class SerberDataManager
+    static class SerberDataManager
     {
+        public static HttpClient HttpClient { get; private set; } = new HttpClient();
+
+        public static string Uri { get; set; } = "";
+        
+        public static bool Upload<T>(T value, string path)
+        {
+            //HttpClient.PostAsJsonAsync("api/products", value);
+            return false;
+        }
+        
+        public static bool Download<RetT>(out RetT value, string path) where RetT : class
+        {
+            value = null;
+            return false;
+        }
+
+        public static bool Download<RetT,ArgT>(ArgT arg, out RetT value, string path) where RetT : class
+        {
+            value = null;
+            return false;
+        }
     }
 }
