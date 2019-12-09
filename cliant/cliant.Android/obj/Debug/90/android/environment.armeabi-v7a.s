29a429f93f4423d60a99eae77f4913f4f265b88b
Hiro <Hiro@STB1018113>
2019-12-09 13:40:37 +0900

    Xamarin.Formsのバージョンアップ

--- cliant/cliant.Android/obj/Debug/90/android/environment.armeabi-v7a.s
+++ cliant/cliant.Android/obj/Debug/90/android/environment.armeabi-v7a.s
@@ -82,7 +82,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",%progbits,1
 	.type	.L.str.8, %object
 .L.str.8:
-	.asciz	"c15c09f2-921c-4104-98ca-55697a94e8e5"
+	.asciz	"764b9240-1721-4cf7-a3cb-c96c28e49716"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",%progbits,1
 	.type	.L.str.9, %object
