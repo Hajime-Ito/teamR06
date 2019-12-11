4c246621bd0fdd12a38d71ad54e052252c8b481a
hirohiroj3cub <hirohiro_j3cub@yahoo.co.jp>
2019-12-12 01:47:38 +0900

    Get系を実装

--- cliant/cliant/BluetoothManager.cs
+++ cliant/cliant/BluetoothManager.cs
@@ -1,37 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Text;
+using Plugin.BluetoothLE;
+using System.Runtime.InteropServices;
+using System.Reactive.Linq;
+using Newtonsoft.Json;
 
 namespace cliant
 {
-    static class BluetoothManager
+    class BluetoothManager
     {
-        static public string[] GetIDs()
+        static class Guids
+        {
+            public static readonly Guid service = new Guid("cb04fe4d-5229-48b5-87b4-6fdf0be3d985");
+            public static readonly Guid ids = new Guid("00000000-0000-0000-0000-000000000000");
+            public static readonly Guid reqIDs = new Guid("00000000-0000-0000-0000-000000000001");
+            public static readonly Guid datas = new Guid("00000000-0000-0000-0000-000000000002");
+        }
+
+        public string[] GetIDs()
+        {
+            List<string> ids = new List<string>();
+
+            CrossBleAdapter.Current.Scan().Subscribe(scanResult =>
+            {
+                // Once finding the device/scanresult you want
+                scanResult.Device.Connect();
+
+                scanResult.Device.GetKnownCharacteristics(Guids.service, new Guid[] { Guids.ids }).Subscribe(async characteristic =>
+                {
+                    var read = await characteristic.Read();
+                    ids.Add(Encoding.UTF8.GetString(read.Data));
+                });
+
+            });
+
+            return ids.ToArray();
+        }
+
+        public string[] GetReqIDs()
+        {
+            List<string> reqIDs = new List<string>();
+
+            CrossBleAdapter.Current.Scan().Subscribe(scanResult =>
+            {
+                // Once finding the device/scanresult you want
+                scanResult.Device.Connect();
+
+                scanResult.Device.GetKnownCharacteristics(Guids.service, new Guid[] { Guids.reqIDs }).Subscribe(async characteristic =>
+                {
+                    var read = await characteristic.Read();
+                    reqIDs.Add(Encoding.UTF8.GetString(read.Data));
+                });
+
+            });
+
+            return reqIDs.ToArray();
+        }
+
+        public FlyerData[] GetDatas()
+        {
+            List<FlyerData> datas = new List<FlyerData>();
+
+            CrossBleAdapter.Current.Scan().Subscribe(scanResult =>
+            {
+                // Once finding the device/scanresult you want
+                scanResult.Device.Connect();
+
+                scanResult.Device.GetKnownCharacteristics(Guids.service, new Guid[] { Guids.datas }).Subscribe(async characteristic =>
+                {
+                    var read = await characteristic.Read();
+                    var str = Encoding.UTF8.GetString(read.Data);
+                    datas.Add(JsonConvert.DeserializeObject<FlyerData>(str));
+                });
+
+            });
+
+            return datas.ToArray();
+        }
+
+
+
+        public void SendIDs(string[] ids)
         {
             throw new NotImplementedException();
         }
 
-        static public string[] GetReqIDs()
+        public void SendReqIDs(string[] reqIds)
         {
             throw new NotImplementedException();
         }
 
-        static public FlyerData[] GetDatas()
-        {
-            throw new NotImplementedException();
-        }
-
-        static public void SendIDs(string[] ids)
-        {
-            throw new NotImplementedException();
-        }
-
-        static public void SendReqIDs(string[] reqIds)
-        {
-            throw new NotImplementedException();
-        }
-
-        static public void SendDatas(FlyerData[] flyerDatas)
+        public void SendDatas(FlyerData[] flyerDatas)
         {
             throw new NotImplementedException();
         }
