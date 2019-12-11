a63df20f0c64c73ca7e5f3ae96c3677c01e70ab8
hebereke <antny0202@gmail.com>
2019-12-11 19:27:00 +0900

    チラシのデータクラスを追加

---
+++ cliant/cliant/FlyerData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace cliant
+{
+    enum FlyerCategory
+    {
+
+    }
+    class FlyerData
+    {
+        DateTime date { set; get; }
+        string text { set; get; }
+        double locationX { set; get; }
+        double locationY { set; get; }
+        string locationName { set; get; }
+        FlyerCategory category { set; get; }
+        Stream attachedFile { set; get; }
+        string ID { set; get; }
+
+        public bool EqualsIdentity(FlyerData flyerData)
+        {
+            if (flyerData == null)
+            {
+                return false;
+            }
+
+            return (this.ID == flyerData.ID);
+        }
+
+        void ContentUpdate()
+        {
+            throw new NotImplementedException("ContentUpdateは未実装です");
+        }
+
+
+
+    }
+}
