b8289a651a37cd634550f0b3ea7a6de188b749ac
Hajime <b1018194@fun.ac.jp>
2019-12-11 17:28:37 +0900

    update ./js/hotspot.js

--- server/js/hotspot.js
+++ server/js/hotspot.js
@@ -3,21 +3,26 @@ b1018194 Ito Hajime
 HotSpot.js
 hotspotを求める
 */
-let mylocate = { x: 0, y: 0 }
-let locateobj = [{ x: 1, y: 0 }, { x: 1, y: 1.5 }, { x: 2, y: 2 },
+
+const mylocate = { x: 0, y: 0 } // 自分自身の位置
+const locateuser = [{ x: 1, y: 0 }, { x: 1, y: 1.5 }, { x: 2, y: 2 },
 { x: 3, y: 1 }, { x: 1, y: 3 }, { x: 3, y: 3 },
-{ x: 3, y: 2.5 }, { x: 2, y: 3 }, { x: 5, y: 5 }, { x: 1, y: 1.7 }]
+{ x: 3, y: 2.5 }, { x: 2, y: 3 }, { x: 5, y: 5 }, { x: 1, y: 1.7 }] // それぞれのユーザの位置情報
+const distancefromMe = 10 // 自分の位置から取得するhotspotの最大距離(半径)
+const distance_min = 1.2 // それぞれのユーザ位置情報の最小直線距離
 
-const distancefromMe = 10
-const distance_min = 1.2
-let obj = []
+let locateobj = locateuser
+let obj = [] // 条件を満たすユーザ位置情報を管理する
+let HotSpot = [] //HotSpot
 
+// 一定の距離内にあって、かつ近い距離にユーザ位置情報があるユーザ位置情報をまとめて(numで管理して)obj配列に挿入する
 for (let i = 0; i < locateobj.length; i++) {
     let x1 = locateobj[i].x
     let y1 = locateobj[i].y
-    if (Math.sqrt((x1 - mylocate.x) ^ 2 + (y1 - mylocate.y) ^ 2) > distancefromMe) {
-        let idx = locate.indexOf({ x: x1, y: y1 })
-        locate.splice(idx, 1)
+    if (Math.sqrt((x1 - mylocate.x) * (x1 - mylocate.x) + (y1 - mylocate.y) * (y1 - mylocate.y)) > distancefromMe) {
+        // 取得する最大距離以上の距離にあるユーザ位置情報である
+        let idx = locate.indexOf({ x: x1, y: y1 }) // 各当情報を検索
+        locate.splice(idx, 1) //それを削除
         break
     }
     for (let j = 0; j < locateobj.length; j++) {
@@ -26,37 +31,35 @@ for (let i = 0; i < locateobj.length; i++) {
         if (i == j) break
         if (Math.abs((Math.sqrt((x1 - mylocate.x) * (x1 - mylocate.x) + (y1 - mylocate.y) * (y1 - mylocate.y))) - (Math.sqrt((x2 - mylocate.x) * (x2 - mylocate.x) + (y2 - mylocate.y) * (y2 - mylocate.y)))) < distance_min) {
             if (Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) < distance_min) {
-                //console.log(Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)))
-                obj.push({ x1: x1, y1: y1, x2: x2, y2: y2, num: i })
+                // ユーザ位置情報(x1,y1)と(x2,y2)の距離が近いか(distance_min以内)
+                /*console.log(Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)))*/
+                obj.push({ x: x2, y: y2, num: i }) // 各当情報をobj配列に挿入 
             }
         }
     }
 }
 
-let tmpcount = 0
-let tmpx = 0
-let tmpy = 0
-let count = 1
-let tmpArray = []
-
 for (let i = 0; i < locateobj.length; i++) {
-    let bool = false
+    let tmpx = 0
+    let tmpy = 0
+    let count = 0 // numが何回検出されたか(グループに何個位置情報が存在したか)
+    let bool = false // i番目は仲間を持つ(一定の距離内にユーザ位置情報)があるかどうか
     for (let j = 0; j < obj.length; j++) {
         if (i == obj[j].num) {
+            // numで管理しているグループ情報が一致した場合(num = i)
             count++
-            bool = true
-            tmpx += obj[j].x2
-            tmpy += obj[j].y2
+            bool = true // 仲間を持つのでtrue
+            tmpx += obj[j].x
+            tmpy += obj[j].y
         }
     }
     if (bool) {
-        tmpx += locateobj[i].x
-        tmpy += locateobj[i].y
-        tmpArray.push({ hotx: tmpx / count, hoty: tmpy / count, count: count })
-        count = 1
-        bool = false
+        tmpx += locateobj[i].x //仲間を持つ基準位置となったユーザ位置情報x
+        tmpy += locateobj[i].y //仲間を持つ基準位置となったユーザ位置情報y
+        HotSpot.push({ hotx: tmpx / (count + 1), hoty: tmpy / (count + 1), count: count + 1 })
+        // Hotspot配列にグループの平均位置を保存(count+1となっているのは基準ユーザ位置の分がカウントされていないため)
     }
 }
 
 console.log(obj)
-console.log(tmpArray)
+console.log(HotSpot)
