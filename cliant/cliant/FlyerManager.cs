472298d7f39ceaa9aff813b9c5ca0f639118e5c6
hebereke <antny0202@gmail.com>
2019-12-12 12:51:30 +0900

    FlyerManagerをちょっといじった

---
+++ cliant/cliant/FlyerManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cliant
+{
+    class FlyerManager
+    {
+        //myFlyerDatasから自分が持っているIDを求める
+        List<FlyerData> myFlyerDatas = new List<FlyerData>();
+        List<string> yourFlyerIDs = new List<string>();
+        List<string> reqFlyerIDs = new List<string>();
+
+        void SendManagerData()
+        {
+            string[] myFlyerDatasA = { };
+            string[] yourFlyerIDsA;
+            string[] reqFlyerIDsA;
+
+            BluetoothManager.SendIDs(myFlyerDatasA);
+
+        }
+
+        void GetManagerData()
+        {
+
+        }
+
+
+    }
+}
