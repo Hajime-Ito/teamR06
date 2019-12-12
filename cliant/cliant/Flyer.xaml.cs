3227c06559a66bfeb3eac29ecd98684fd1e99f72
Melujiro <haradario2014@gmail.com>
2019-12-12 17:52:50 +0900

    テキストのリスト追加

--- cliant/cliant/Flyer.xaml.cs
+++ cliant/cliant/Flyer.xaml.cs
@@ -12,9 +12,17 @@ namespace cliant
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Flyer : ContentPage
     {
+        List<FlyerItem> flyers = new List<FlyerItem>();
+
         public Flyer()
         {
             InitializeComponent();
+            flyers.Add(new FlyerItem("FUNクリスマス会","函館","2020年1月10日","サンプルテキスト", "イベント") );
+            flyers.Add(new FlyerItem("FUNクリスマス会", "函館", "2020年1月10日", "サンプルテキスト", "イベント"));
+            flyers.Add(new FlyerItem("FUNクリスマス会", "函館", "2020年1月10日", "サンプルテキスト", "イベント"));
+            flyers.Add(new FlyerItem("FUNクリスマス会", "函館", "2020年1月10日", "サンプルテキスト", "イベント"));
+
+            FlyerList.ItemsSource = flyers;
         }
     }
 }
