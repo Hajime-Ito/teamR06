14fbc3f8a1205a014784b209c7c8c3e80f574450
hebereke <antny0202@gmail.com>
2019-12-14 11:51:47 +0900

    PartyDataクラスの作成

---
+++ cliant/cliant/PartyData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cliant
+{
+    enum Type
+    {
+
+    }
+    class PartyData
+    {
+        int Date { set; get; }
+        int Month { set; get; }
+        int Year { set; get; }
+        string Owner { set; get; }
+        string Message { set; get; }
+        Type Type { set; get; }
+        double LocationX { set; get; }
+        double LocationY { set; get; }
+
+    }
+}
