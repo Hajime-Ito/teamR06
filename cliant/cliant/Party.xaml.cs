8c634a2720e66aa612abc34ead3fa533e25c7229
Melujiro <haradario2014@gmail.com>
2019-12-15 07:58:48 +0900

    Party.xamlを追加

---
+++ cliant/cliant/Party.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace cliant
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    Party_View<PartyRet> partys = new Party_View<PartyRet>()
+    public partial class Party : ContentPage
+    {
+        public Party()
+        {
+            InitializeComponent();
+        }
+    }
+}
