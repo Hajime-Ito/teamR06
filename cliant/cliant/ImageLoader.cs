e341dbcc009b3325aaa4414ccfc5fc77448b12d2
hirohiroj3cub <hirohiro_j3cub@yahoo.co.jp>
2019-12-15 02:40:48 +0900

    圧縮に対応（できなかった）

--- cliant/cliant/ImageLoader.cs
+++ cliant/cliant/ImageLoader.cs
@@ -3,24 +3,22 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using Plugin.Media.Abstractions;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using System.IO.Compression;
 
 namespace cliant
 {
     public class Image
     {  
-        public Image(Stream stream)
+        public Image(MemoryStream stream)
         {
             Stream = stream;
         }
 
-        public Stream Stream { get; }
+        public MemoryStream Stream { get; set; }
 
         public ImageSource ImageSouce
         {
@@ -35,9 +33,12 @@ namespace cliant
             byte[] bytes = null;
             using (var memoryStream = new MemoryStream())
             {
+                Stream.Position = 0;
                 Stream.CopyTo(memoryStream);
                 bytes = memoryStream.ToArray();
+                Stream.Position = 0;
             }
+
 
            return Convert.ToBase64String(bytes);
         }
@@ -46,21 +47,50 @@ namespace cliant
         {
             return new Image(new MemoryStream(Convert.FromBase64String(str)));
         }
+
+        public string ToCompBase64String()
+        {
+            byte[] bytes = null;
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                using (GZipStream compressionStream = new GZipStream(outStream, CompressionMode.Compress))
+                {
+                    Stream.Position = 0;
+                    Stream.CopyTo(compressionStream);
+                    bytes = outStream.ToArray();
+                    Stream.Position = 0;
+                }
+            }
+
+            return Convert.ToBase64String(bytes);
+        }
+
+        public static Image FromCompBass64String(string str)
+        {
+            var input = new MemoryStream(Convert.FromBase64String(str));
+            Image image = null;
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                using (GZipStream compressionStream = new GZipStream(outStream, CompressionMode.Decompress))
+                {
+                    input.CopyTo(compressionStream);
+                    image = new Image(outStream);
+                }
+            }
+
+            return image;
+        }
     }
 
     public static class ImageLoader
     {
-        public static bool TryLoad(out Image image)
+        public static void LoadImage(Action<Image> loadAction )
         {
-            bool ret = false;
-            var roadImage = new Image(null);
             DependencyService.Get<ImageViewer.Services.IImageService>().ShowImageGallery((newimage) => {
-                ret = true;
-                roadImage = new Image(newimage);
+                var stream = new MemoryStream();
+                newimage.CopyTo(stream);
+                loadAction(new Image(stream));
             });
-
-            image = roadImage;
-            return ret;
         }
     }
 }
