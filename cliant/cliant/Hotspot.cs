0601046af19cf997069a2983c35d97462c5bdd9d
hebereke <antny0202@gmail.com>
2019-12-14 12:55:19 +0900

    Hotspotクラスの追加

---
+++ cliant/cliant/Hotspot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cliant
+{
+    class Hotspot
+    {
+        double locationX;
+        double locationY;
+        int size;
+    }
+}
